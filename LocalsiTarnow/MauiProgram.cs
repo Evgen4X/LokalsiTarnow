5e9d03d9769a427f5515a210fab95887941cec9c
Evgen4X <y.maskaiev.5555@gmail.com>
2025-06-05 13:37:35 +0200

    f

--- LocalsiTarnow/MauiProgram.cs
+++ LocalsiTarnow/MauiProgram.cs
@@ -15,6 +15,8 @@ namespace LocalsiTarnow
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
                 });
 
+            builder.Services.AddTransient<MainPage>();
+
 #if DEBUG
     		builder.Logging.AddDebug();
 #endif
